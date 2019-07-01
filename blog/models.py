282c8537d12e0289f67abb77f64149d8fa12f108
linda <1936074835@qq.com>
2019-07-01 22:54:07 +0800

    数据模型：创建博客文章数据模型

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    # 唯一ID标记
+    article_id = models.AutoField(primary_key=True)
+    # 文章标题
+    title = models.TextField()
+    # 文章摘要
+    brief_content = models.TextField()
+    # 文章内容
+    content = models.TextField()
+    # 发布日期
+    publish_date = models.DateTimeField(auto_now=True)
